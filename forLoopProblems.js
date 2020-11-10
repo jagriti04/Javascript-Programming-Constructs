113a80917788565912748d4e185f25c585538d55
Jagriti <jagriti.agni@gmail.com>
2020-11-11 02:18:07 +0530

    [Jagriti]ADDED: Using for loop print nth harmonic term

--- forLoopProblems.js
+++ forLoopProblems.js
@@ -2,8 +2,18 @@ const prompt = require('prompt-sync')();
 
 // Problem 1 - numbers less than 2^n
 {
-    const n = parseInt(prompt("Enter a number: "));
+    const n = parseInt(prompt("Enter a number to find power of 2: "));
     for(let i=0; i <= n; i++) {
         console.log("Number "+i + "th place is "+ Math.pow(2, i));
     }
+}
+
+// Problem 2 - Harmonic Number
+{
+    const num = parseInt(prompt("Enter a number to find nth harmonic term: "));
+    let harmonicNum = 0;
+    for(let i=1; i <= num; i++) {
+        harmonicNum = harmonicNum + (1/i);
+    }
+    console.log("nth term is: "+harmonicNum);
 }
