9ebdabbb61829892cdde1ffcb8a917ab406b0bd9
Jagriti <jagriti.agni@gmail.com>
2020-11-11 02:26:38 +0530

    [Jagriti]ADDED: Using for loop to check if a number is prime

--- forLoopProblems.js
+++ forLoopProblems.js
@@ -3,8 +3,8 @@ const prompt = require('prompt-sync')();
 // Problem 1 - numbers less than 2^n
 {
     const n = parseInt(prompt("Enter a number to find power of 2: "));
-    for(let i=0; i <= n; i++) {
-        console.log("Number "+i + "th place is "+ Math.pow(2, i));
+    for (let i = 0; i <= n; i++) {
+        console.log("Number " + i + "th place is " + Math.pow(2, i));
     }
 }
 
@@ -12,8 +12,24 @@ const prompt = require('prompt-sync')();
 {
     const num = parseInt(prompt("Enter a number to find nth harmonic term: "));
     let harmonicNum = 0;
-    for(let i=1; i <= num; i++) {
-        harmonicNum = harmonicNum + (1/i);
+    for (let i = 1; i <= num; i++) {
+        harmonicNum = harmonicNum + (1 / i);
     }
-    console.log("nth term is: "+harmonicNum);
+    console.log("nth term is: " + harmonicNum);
+}
+
+// Problem 3 - Prime Number
+{
+    const num = parseInt(prompt("Enter a number to find if number is prime: "));
+    let isPrime = true;
+    if (num == 1) {
+        console.log("number is neither prime nor composite");
+    } else if (num > 1) {
+        for (let i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+            }
+        }
+        console.log(num + " is prime number - " + isPrime);
+    }
 }
