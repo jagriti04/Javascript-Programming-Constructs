ae7623499b412be75f166759fda60978bec76ab7
Jagriti <jagriti.agni@gmail.com>
2020-11-11 01:39:01 +0530

    [Jagriti]ADDED:code if-else-if problem 1, get number input and show week day as output

--- selectionProblemIfElseIf.js
+++ selectionProblemIfElseIf.js
@@ -24,3 +24,22 @@ const prompt = require('prompt-sync')();
     else if (number == 9)
         console.log("number is: nine");
 }
+
+// Problem 2 - number to week day
+{
+    let number = prompt("Enter a single digit number (1-7): ");
+    if (number == 1)
+        console.log("day is: Sunday");
+    else if (number == 2)
+        console.log("day is: Monday");
+    else if (number == 3)
+        console.log("day is: Tuesday");
+    else if (number == 4)
+        console.log("day is: Wednesday");
+    else if (number == 5)
+        console.log("day is: Thurday");
+    else if (number == 6)
+        console.log("day is: Friday");
+    else if (number == 7)
+        console.log("day is: Saturday");
+}
