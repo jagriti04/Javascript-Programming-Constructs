79fed9e27326ab1535b74db5485151248fd037f9
Jagriti <jagriti.agni@gmail.com>
2020-11-11 00:55:03 +0530

    [Jagriti]ADDED:code if-else problem 2, check if input date is in range

--- selectionProblemIfElse.js
+++ selectionProblemIfElse.js
@@ -15,3 +15,28 @@
     console.log("Minimum value is- " + minValue);
     console.log("Maximum value is- " + maxValue);
 }
+
+// Problem 2 -Day and month range check
+{
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let isInDateRange = false;
+    readline.question('Enter a month: ', (month) => {
+        readline.question('Enter day: ', (day) => {
+            if (month == 'March' && day >= 20) {
+                isInDateRange = true;
+            }
+            if (month == 'April' || month == 'May') {
+                isInDateRange = true;
+            }
+            if (month == 'June' && day <= 20) {
+                isInDateRange = true;
+            }
+            console.log("The given day and month is in range - " + isInDateRange);
+            readline.close();
+        })
+    });
+}
