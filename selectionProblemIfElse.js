623c6b873a5eff89368eb7cbcf6e89e73ac89f4e
Jagriti <jagriti.agni@gmail.com>
2020-11-11 01:11:42 +0530

    [Jagriti]ADDED:code if-else problem 3, check if given year is leap year

--- selectionProblemIfElse.js
+++ selectionProblemIfElse.js
@@ -16,27 +16,37 @@
     console.log("Maximum value is- " + maxValue);
 }
 
+const prompt = require('prompt-sync')({sigint: true});
 // Problem 2 -Day and month range check
 {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
+    let month = prompt('Enter a month: ');
+    let day = prompt('Enter day: ');
     let isInDateRange = false;
-    readline.question('Enter a month: ', (month) => {
-        readline.question('Enter day: ', (day) => {
-            if (month == 'March' && day >= 20) {
-                isInDateRange = true;
+    if (month == 'March' && day >= 20) {
+        isInDateRange = true;
+    }
+    if (month == 'April' || month == 'May') {
+        isInDateRange = true;
+    }
+    if (month == 'June' && day <= 20) {
+        isInDateRange = true;
+    }
+    console.log("The given day and month is in range - " + isInDateRange);
+}
+
+// Problem 3 -check if leap year
+{
+    let year = prompt("Enter the year: ");
+    let isLeapYear = false;
+    if (year > 999 && year <= 9999) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                isLeapYear = true;
             }
-            if (month == 'April' || month == 'May') {
-                isInDateRange = true;
+            if (year % 400 == 0) {
+                isLeapYear = true;
             }
-            if (month == 'June' && day <= 20) {
-                isInDateRange = true;
-            }
-            console.log("The given day and month is in range - " + isInDateRange);
-            readline.close();
-        })
-    });
+        }
+    }
+    console.log("Is the given year a leap year- " + isLeapYear);
 }
