8c233e2244c02278afb8d6f72433097c1abc536f
Jagriti <jagriti.agni@gmail.com>
2020-11-12 00:43:55 +0530

    [Jagriti]ADDED: Code to find repeated numbers in an array

--- arrayPracticeProblem.js
+++ arrayPracticeProblem.js
@@ -88,3 +88,22 @@ function checkIfSumZero(numArray) {
     let numArray = [1, -2, 3, 4, -5, -3, 2];
     checkIfSumZero(numArray);
 }
+
+// Problem 5- find the digits that are repeated twice
+function findRepeatedNumber(numArray) {
+    let arrayLength = numArray.length;
+    let repeatedNum = [];
+    for (let i = 0; i < arrayLength; i++) {
+        let tens = numArray[i] % 10;
+        let ones = Math.floor(numArray[i] /10);
+        if (tens == ones) {
+            repeatedNum.push(numArray[i]);
+        }
+    }
+    console.log("Numbers: "+numArray);
+    console.log("Repeated numbers: "+repeatedNum);
+}
+{
+    let numArray = [12, 23, 33, 54, 55, 88, 17];
+    findRepeatedNumber(numArray);
+}
