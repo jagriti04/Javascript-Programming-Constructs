cc21e035b43e4c82c88bbe14b0379d5d40bbdef8
Jagriti <jagriti.agni@gmail.com>
2020-11-12 01:51:02 +0530

    [Jagriti]ADDED: Code for creating map for people with same birth month

--- mapsProblem.js
+++ mapsProblem.js
@@ -35,3 +35,20 @@ function findKeyWithMinValue(diceMap) {
     }
     console.log("Number that reached minimum times: " + findKeyWithMinValue(diceValueTimes));
 }
+
+// Problem 2- birth month
+{
+    let birthMonthMap = new Map();
+    let peopleNum = 50;
+    for (let i = 1; i <= 12; i++) {
+        birthMonthMap.set(i, 0);
+    }
+    while(peopleNum>=0) {
+        let monthNum = getRandomInt(1, 12);
+        let value = birthMonthMap.get(monthNum);
+        value++;
+        birthMonthMap.set(monthNum, value);
+        peopleNum--;
+    }
+    console.log(birthMonthMap);
+}
