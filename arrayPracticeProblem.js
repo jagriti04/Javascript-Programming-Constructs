def608d43fbce2877b9d27a2c2b4c42f399325b7
Jagriti <jagriti.agni@gmail.com>
2020-11-12 00:30:33 +0530

    [Jagriti]ADDED: Code to find triplets with sum zero in an array

--- arrayPracticeProblem.js
+++ arrayPracticeProblem.js
@@ -63,5 +63,28 @@ function getPrimeFactors(num) {
     return factorsArray;
 }
 
-console.log("Prime factors of 323- "+ getPrimeFactors(323));
-console.log("Prime factors of 75- "+ getPrimeFactors(75));
+console.log("Prime factors of 323- " + getPrimeFactors(323));
+console.log("Prime factors of 75- " + getPrimeFactors(75));
+
+// Problem 4 - to show sum of three integers is zero
+function checkIfSumZero(numArray) {
+    const arrayLength = numArray.length;
+    let isSumZero = false;
+    for (let i = 0; i < arrayLength - 2; i++) {
+        for (let j = i + 1; j < arrayLength - 1; j++) {
+            for (let k = j + 1; k < arrayLength; k++) {
+                if (numArray[i] + numArray[j] + numArray[k] == 0) {
+                    console.log("Triplet with sum zero: " + numArray[i] + " " + numArray[j] + " " + numArray[k]);
+                    isSumZero = true;
+                }
+            }
+        }
+    }
+    if (isSumZero == false) {
+        console.log("No such triplet exists");
+    }
+}
+{
+    let numArray = [1, -2, 3, 4, -5, -3, 2];
+    checkIfSumZero(numArray);
+}
