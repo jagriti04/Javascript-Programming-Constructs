d64a7e89e1cdada0aa509df9cbb69bff5d539f22
Jagriti <jagriti.agni@gmail.com>
2020-11-11 23:06:53 +0530

    [Jagriti]ADDED: Code to find second max and min element by sorting array

--- arrayPracticeProblem.js
+++ arrayPracticeProblem.js
@@ -5,6 +5,7 @@
         numArray[i] = Math.floor(Math.random() * 900) + 100;
     }
     console.log("Array is- " + numArray);
+    // second min and max without sorting
     let secondMax = findSecondMaxWithoutSorting(numArray);
     console.log("Second largest num is- " + secondMax);
     let secondMin = findSecondMinWithoutSorting(numArray);
@@ -21,4 +22,22 @@ function findSecondMinWithoutSorting(array) {
     let min = Math.min(...array);
     array.splice(array.indexOf(min), 1);
     return Math.min(...array);
+}
+
+// Problem 2
+// second min and max with sorting (problem 2)
+{
+    let sortArray = [];
+    for (let i = 0; i < 10; i++) {
+        sortArray[i] = Math.floor(Math.random() * 900) + 100;
+    }
+    console.log("Array is (with sorting)- " + sortArray);
+    findSecondMaxMinWithSorting(sortArray); 
+}
+
+function findSecondMaxMinWithSorting(array) {
+    array.sort();
+    console.log("sorted array "+array);
+    console.log("Second largest num is- " + array[8]);
+    console.log("Second smallest num is- " + array[1]);
 }
